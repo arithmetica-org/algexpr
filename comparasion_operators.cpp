89664b571655dad0e7c1352a13a5a35609069424
Avighna <avighnakc@gmail.com>
2025-02-17 05:43:55 +0530

    Fixed == operator

--- comparasion_operators.cpp
+++ comparasion_operators.cpp
@@ -2,12 +2,6 @@
 
 namespace algexpr {
 bool operator==(const algexpr &a, const algexpr &b) {
-  // this is NOT how you should be doing it, but i am temporarily cuz i havent
-  // coded simplification yet
-
-  // ideally, you'd wanna create algexpr(a-b).simplify() and check if THIS is
-  // equal to 0. but for now, we're just gonna do a.to_string() == b.to_string()
-
-  return a.to_string() == b.to_string();
+  return (a - b).simplify().to_string() == "0";
 }
 } // namespace algexpr
